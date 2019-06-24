c4861c49906ad5e275bec10a32f0512c626781fb
Andrey Miroshnichenko <miron152008@gmail.com>
2019-06-24 09:09:02 +0300

    done join

--- join.js
+++ join.js
@@ -1,8 +1,37 @@
 function join(array, separator) {
 
+    let str = "";
+    let separatorStr = "" + separator;
+
+    if (array.length == 0) {
+        str = "";
+    }
+
+    else if (!separator){
+        for (let i = 0; i < array.length; i++) {
+            str = str + array[i];
+            if (i < array.length - 1)
+                str = str + ",";
+        }
+    }
+
+    else {
+        for (let i = 0; i < array.length; i++) {
+            str = str + array[i];
+            if (i < array.length - 1)
+                str = str + separatorStr;
+        }
+    }
+    return str;
 }
 
 
 const array = ['Lorem', 'ipsum', 'dolor', 'sit', 'amet', 'consectetur', 'adipiscing', 'elit'];
 
-console.log(join(array, ' '));
+console.log(join(array, '-'));
+
+//all checks
+console.log(join([], ' ')); //if empty array
+console.log(join(array)); //if empty separator
+console.log(join(array, 0)); //if separator is number
+
