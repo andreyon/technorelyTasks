f901f537c7df75ba8592900afd6505755700f68e
Andrey Miroshnichenko <miron152008@gmail.com>
2019-07-08 21:26:22 +0300

    add try-catch to asyncAwait

--- asyncAwait.js
+++ asyncAwait.js
@@ -26,28 +26,34 @@ function test() {
 }
 
 async function asyncTest() {
+    let user;
+    let post;
     console.log('I have to appear in a console first');
 
-    let response = await fetch('https://jsonplaceholder.typicode.com/users');
-    let res = await response.json();
-    let user = await res[random(10, 0)];
-    console.log('My user is', user);
+    try {
+        let response = await fetch('https://jsonplaceholder.typicode.com/users');
+        let res = await response.json();
+        user = await res[random(10, 0)];
+        console.log('My user is', user);
+    } catch(error) {
+        throw new Error("Не удалось получить данные пользователя");
+    }
 
+    try {
     response = await fetch('https://jsonplaceholder.typicode.com/posts');
     res = await response.json();
-    let post = await res[random(10, 0)];
+    post = await res[random(10, 0)];
     console.log('My post is', post);
+    } catch(error) {
+        throw new Error("Не удалось получить данные поста");
+    }
 
     const finalRes = await {
         user,
         post
     };
-        console.log('Finally', finalRes);
+    console.log('Finally', finalRes);
 }
 
 // test();
 asyncTest();
-
-
-/*
-В файле 'asyncAwait.js' переписать предложенную вам функцию test. Используя async await. Последовательность результатов вывода в консоль должна быть такой же как и в предложенном варианте.*/
