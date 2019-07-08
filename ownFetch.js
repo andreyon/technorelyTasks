001c47f0a98c1b26baa0a38c38107e819b57d390
Andrii Miroshnychenko <38904960+andreyon@users.noreply.github.com>
2019-07-08 10:20:34 +0300

    new version ownFetch

--- ownFetch.js
+++ ownFetch.js
@@ -1,27 +1,23 @@
-const ownFetch = (url) => {
+const ownFetch = url => {
+  return new Promise((resolve, reject) => {
+    let response = new XMLHttpRequest();
+    response.open("GET", url, true);
 
-   let response = new XMLHttpRequest();
-    response.open('GET', url, true);
+    response.onload = function() {
+      if (!response) {
+        let err = new Error("connection problems: no response received");
+        reject(err);
+      } else if (response.status !== 200) {
+        resolve(alert(response.status + ": " + response.statusText));
+      } else {
+        resolve(response);
+      }
+    };
     response.send();
-
-    return new Promise((resolve, reject) => {
-        response.onreadystatechange = function () {
-
-            if (!response) {
-                let err = new Error('connection problems: no response received');
-                reject(err);
-            }
-            else if (response.status !== 200) {
-                resolve(alert(response.status + ": " + response.statusText))
-            }
-            else {
-                resolve(response);
-            }
-        }
-    });
+  });
 };
 
-ownFetch('https://jsonplaceholder.typicode.com/posts')
-    .then(console.log)
-    .then(res => res())
-    .catch(err => console.log(err));
+ownFetch("https://jsonplaceholder.typicode.com/posts")
+  .then(res => res.responseText)
+  .then(console.log)
+  .catch(err => console.log(err));
